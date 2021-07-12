d100cd8438cac7fd35bd84a130d214fd30277d1b
Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
2021-07-12 19:06:36 +0800

    ima sign

--- ima/keyadd.sh
+++ ima/keyadd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-keyctl padd asymmetric "IMA-SM2" %:.secondary_trusted_keys < cert.der
+#keyctl padd asymmetric "IMA-SM2" %:.secondary_trusted_keys < cert.der
+
+keyctl padd asymmetric "IMA-SM2" %:.ima < cert.der
 
 keyctl show %:.secondary_trusted_keys
+keyctl show %:.ima
---
+++ ima/sign.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+evmctl ima_sign --hashalgo sm3 --imasig --key private.pem hello
